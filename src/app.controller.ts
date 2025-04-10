01d9ab47b342febcb5adcfaf814fdbf2b5e671b3
YuriAlves25 <yuri_alves00@hotmail.com>
2025-04-09 21:10:42 -0300

    Prisma setup and connected to MongoDB

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './DataBase/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  async getHello() {
+    const user = await this.prisma.user.create({
+      data: {
+        username: 'yuri',
+        email: 'yur1@hotmail.com',
+        password: '12345',
+      }
+    })
+  return user}
 }
